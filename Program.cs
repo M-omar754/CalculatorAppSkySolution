af2c8041cbafa2852296b8a002d4f5b6e7abc178
mohammad mresh <mo2000624@gmail.com>
2024-12-25 20:35:47 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿
+public class Calculator
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    public Calculator(double x, double y)
+    {
+        X = x; Y = y;
+    }
+    public double Add()
+    {
+        return X + Y;
+    }
+
+    public double Sub()
+    {
+        return Y - X;
+    }
+
+    public double Mul()
+    {
+        return X * Y;
+    }
+
+    public string Div()
+    {
+        if (Y == 0)
+        {
+            return "Cannot divided by zero";
+        }
+        return (X/Y).ToString();
+
+    }
+}
+class Program {
+    static void Main(string[] args) {
+        Console.WriteLine("Enter X :");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("Enter Y :");
+        double y = Convert.ToDouble(Console.ReadLine());
+        Calculator calculator = new Calculator(x, y);
+
+        Console.WriteLine("\nResults :");
+        Console.WriteLine("Add : " + calculator.Add());
+        Console.WriteLine($"Sub : {calculator.Sub()}");
+        Console.WriteLine($"Mul : {calculator.Mul()}");
+        Console.WriteLine($"Div : {calculator.Div()}");
+
+
+
+    }
+}
+
+
